c3de960e597eb7f3e668fb3b0c6b82f59975a9c3
Nikita Dmitrienko <nikita.d@thrive.io>
2021-10-10 11:59:05 +0300

    Add basic components to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRoute as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Join from './components/Join';
+import Chat from './components/Chat';
 
 const App = () => (
 	<Router>
---
+++ client/src/components/Join.js
@@ -0,0 +1,7 @@
+const Join = () => {
+	return (
+		<h1>Join</h1>
+	);
+};
+
+export default Join;
