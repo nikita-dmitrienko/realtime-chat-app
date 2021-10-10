9b96c7a40c3cda2cd8bcf0e8de1a21804f13c893
Nikita Dmitrienko <nikita.d@thrive.io>
2021-10-10 13:15:54 +0300

    Add style to Join component

--- client/src/components/join/Join.js
+++ client/src/components/join/Join.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import './Join.css';
+
 const Join = () => {
 	const [name, setName] = useState('');
 	const [room, setRoom] = useState('');
