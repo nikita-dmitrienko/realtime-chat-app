405bdad30731cd8f4fc4bd93e71403b487717e6c
Nikita Dmitrienko <nikita.d@thrive.io>
2021-10-10 12:46:43 +0300

    Implement socket connection

--- server/index.js
+++ server/index.js
@@ -4,8 +4,20 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+	console.log('Connection is made');
+
+	socket.on('disconnect', () => {
+		console.log('User had left');
+	});
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
