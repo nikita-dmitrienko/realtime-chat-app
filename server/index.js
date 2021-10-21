0b5f7ce8da1356cfd19e6aafd5201bd8684663a9
Nikita Dmitrienko <nikita.d@thrive.io>
2021-10-21 21:44:46 +0300

    Implement server send messages part

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
 
+
+const {
+	addUser,
+	removeUser,
+	getUser,
+	getUsersByRoom,
+} = require('./users');
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -20,10 +27,41 @@ app.use(router);
 app.use(cors());
 
 io.on('connection', (socket) => {
-	console.log('Connection is made');
-
 	socket.on('join', ({ name, room }, callback) => {
-		console.log(name, room);
+		const { id } = socket;
+
+		const newUser = {
+			id,
+			name,
+			room,
+		};
+
+		const { error, user } = addUser(newUser);
+		if (error) return callback(error);
+
+		socket.emit('message', {
+			user: 'admin',
+			text: `${user.name}, welcome to the room ${user.room}`,
+		});
+
+		socket.broadcast.to(user.room).emit('message', {
+			user: 'admin',
+			text: `${user.name}, has joined!`,
+		});
+
+		socket.join(user.room);
+
+		callback();
+	});
+
+	socket.on('sendMessage', (message, callback) => {
+		const { id } = socket;
+
+		const user = getUser(id);
+		io.to(user.room).emit('message', {
+			user: user.name,
+			text: message,
+		});
 
 		callback();
 	});
