ef03429a81cbe636ff995b5ca45ba10934e5bc92
Nikita Dmitrienko <nikita.d@thrive.io>
2021-10-21 21:58:54 +0300

    Add front end messages handling, add chat styles

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import queryString from 'query-string';
 import { io } from 'socket.io-client';
+import './Chat.css';
 
 let socket;
 
 const Chat = ({ location }) => {
 	const [name, setName] = useState('');
 	const [room, setRoom] = useState('');
+	const [message, setMessage] = useState('');
+	const [messages, setMessages] = useState([]);
 
 	const ENDPOINT = 'localhost:5000';
 
@@ -19,7 +22,7 @@ const Chat = ({ location }) => {
 		setRoom(room);
 
 		socket.emit('join', { name, room }, () => {
-			console.log('The callback is fired!');
+			console.log(name, room);
 		});
 
 		return () => {
@@ -28,8 +31,35 @@ const Chat = ({ location }) => {
 		};
 	}, [ENDPOINT, location.search]);
 
+	useEffect(() => {
+		socket.on('message', (message) => {
+			setMessages([...messages, message]);
+		});
+	}, [messages]);
+	
+	const sendMessage = (e) => {
+		e.preventDefault();
+
+		if (message) {
+			socket.emit('sendMessage', message, () => setMessage(''))
+		}
+	}
+
+	console.log(message, messages);
 	return (
-		<h1>Chat</h1>
+		<div className="outerContainer">
+			<div className="container">
+				<input
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+					onKeyPress={(e) => e.key === 'Enter' ? sendMessage(e) : null}
+				/>
+			{/* <InfoBar room={room} /> */}
+			{/* <Messages messages={messages} name={name} /> */}
+			{/* <Input message={message} setMessage={setMessage} sendMessage={sendMessage} /> */}
+		</div>
+		{/* <TextContainer users={users}/> */}
+	  </div>
 	);
 };
 
